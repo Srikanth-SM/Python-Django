4fc4311263c196aed216daf77705bacbf8de3d54
srikanth <srisiro26.pec@gmail.com>
2018-11-30 12:32:35 +0530

    added get_object_404 error for details view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.shortcuts import render, get_object_or_404 
+from django.http import HttpResponse
 from .models import Question, Choice
 # Create your views here.
 
@@ -8,24 +8,25 @@ def index(request):
     return render(request,"index.html",context={'name':'srikanth'})
 
 def details(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-        # print((questions_list[0].choice_set))
-        # return HttpResponse("You're looking at question %s." % question_id)
-        
-    except Exception as e:
-        print (e)
-        print (dir(e))
-        raise Http404(e)
-    return render(request,"polls/details.html",context={'question':question})
-
+    # try:
+    #     question = Question.objects.get(pk = question_id)
+    # except Exception as e:
+    #     print (e)
+    #     print (dir(e))
+    #     raise Http404(e)
+    # return render(request,"polls/details.html",context={'question':question})
+    
+    
+    # shortcut to above procedure is using get_object_or_404()
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request,'polls/details.html', {'question':question}) 
     
 
 def results(request,question_id):
-    return HttpResponse("You are looking at results of question %s."%question_id)
+    return HttpResponse("You are looking at results of question %s." %question_id)
 
 def votes(request,question_id):
-    return HttpResponse("U are votng on question id %s."%question_id)
+    return HttpResponse("U are votng on question id %s." %question_id)
 
-    return HttpResponse("U are votng on question id %s."%question_id)
+
 
