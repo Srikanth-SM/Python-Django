6131185b086d34a8cf2a71d3063e3c0505370868
srikanth <srisiro26.pec@gmail.com>
2018-12-04 15:30:58 +0530

    completed polls app upto tutorail5

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question, Choice
+
+import json
 # Create your views here.
 
 
@@ -37,14 +39,46 @@ def details(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(
-                            "You are looking at results of question %s."
-                            % question_id
-                       )
+    # return HttpResponse(
+    #                         "You are looking at results of question %s."
+    #                         % question_id
+    #                    )
+    question = Question.objects.get(pk=question_id)
+    question_choices = question.choice_set.all()
+    print(dir(question), "hai")
+    return render(request, "polls/results.html",
+                    {
+                        'question': question,
+                        'choices': question_choices
+                    }
+                )
 
 
 def votes(request, question_id):
-    return HttpResponse("U are votng on question id %s." % question_id)
+    # print(dir(request))
+    # print(request.body.choice)
+    # body_unicode = request
+    # print("body_unicode ", body_unicode)
+    # body = json.loads(body_unicode)
+    # content = body['content']
+    # print(dir(Question))
+    print(request.POST)
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request, "polls/details.html",
+            {
+                'question': question,
+                'error_message': "YOU did not select a choice"
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect('/polls/'+question_id+'/results/')
 
 
 
